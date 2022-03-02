c2f3a6937f1b1f54456dcce4dfde634d51faa122
Perfect <Perfect@DESKTOP-HDR1RMU>
2022-03-02 08:31:46 -0500

    web

---
+++ Elf_BaseFramework/src/test/java/computers/TC_ID_040.java
@@ -0,0 +1,25 @@
+package computers;
+
+import org.testng.annotations.Test;
+
+import genericLibrary.Base_Class;
+import pomRepository.RemoveProductFromCart;
+
+
+public class TC_ID_040 extends Base_Class {
+	
+	@Test
+	 public void removeproduct() {
+	  RemoveProductFromCart cart = new RemoveProductFromCart(driver) ; 
+		 
+	   cart.getAppshoe().click();
+	   cart.getEle().click();
+	   cart.getJns().click();
+	   cart.getBel().click();
+	   cart.getAtb().click();
+	   cart.getCtb().click();
+	   cart.getRfm().click();
+	   cart.getUpc().click();
+	}
+
+}
